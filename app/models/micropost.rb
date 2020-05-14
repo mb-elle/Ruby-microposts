5f2c32d31e6d113a8e3d18215978c9b030d5b50b
m.b <m.9745839.b@gmail.com>
2020-05-14 09:49:02 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reverses_of_favorites, class_name: 'Favorite', dependent: :destroy
+  has_many :likers, through: :reverses_of_favorite, source: :user
+
 end
